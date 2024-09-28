6871f0a0648e4169de998492dae0382b90c8ca7c
Ashe <ashemarsh42@gmail.com>
2024-09-28 20:14:54 +0100

    more google attempts

--- backend/routes/oauth-router.js
+++ backend/routes/oauth-router.js
@@ -17,59 +17,71 @@ const client = new OAuth2Client(GOOGLE_CLIENT_ID)
 //     }
 // }
 
-// oAuthRouter.post('/login', async (req, res) => {
-//     console.log(req.body)
-//     const {token} = req.body
-//     const ticket = await client.verifyIdToken({
-//         idToken: token,
-//         audience: GOOGLE_CLIENT_ID
-//     })
-//     console.log(ticket)
+oAuthRouter.post('/login', async (req, res) => {
+    console.log(req.body)
+    const {token} = req.body
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: GOOGLE_CLIENT_ID
+    })
+    console.log(ticket)
 
-//     const {name, email, picture} = ticket.getPayload()
-//     upsert(users, {name, email, picture})
-//     res.status(201)
-//     res.json({name, email, picture})
-// })
+    const {name, email, picture} = ticket.getPayload()
+    const user = await db.user.upsert({
+        where: {email: email},
+        update: {name, picture},
+        create: {name, email, picture}
+    })
+    req.session.userId = user.id
 
-async function verifyGoogleToken(token) {
-    try {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: GOOGLE_CLIENT_ID
-        })
-        return {payload: ticket.getPayload()}
-    } catch (error) {
-        return {error: "Invalid user detected. Please try again."}
-    }
-}
-
-oAuthRouter.post("/login", async(req, res) => {
-    try {
-        if(req.body.credential){
-            const verificationResponse = await verifyGoogleToken(req.body.credential)
-            if(verificationResponse.error){
-                return res.status(400).json({
-                    message: verificationResponse.error
-                })
-            }
-
-            const profile = verificationResponse?.payload
-
-            res.status(201).json({
-                message: "Login successful",
-                user:{
-                    firstName: profile?.given_name,
-                    lastName: profile?.family_name,
-                    picture: profile?.picture,
-                    email: profile?.email,
-                    token: jwt.sign({email: profile?.email}, process.env.JWT_SECRET, {expiresIn: "1d"})
-                }
-            })
-        }
-    } catch (error) {
-        res.status(500).json({message: error?.message || error})
-    }
+    res.status(201)
+    res.json(user)
 })
+
+oAuthRouter.delete('/logout', async (req, res) => {
+    await req.session.destroy()
+    res.status(200)
+    res.json({message: "Logged out successfully"})
+})
+
+// async function verifyGoogleToken(token) {
+//     try {
+//         const ticket = await client.verifyIdToken({
+//             idToken: token,
+//             audience: GOOGLE_CLIENT_ID
+//         })
+//         return {payload: ticket.getPayload()}
+//     } catch (error) {
+//         return {error: "Invalid user detected. Please try again."}
+//     }
+// }
+
+// oAuthRouter.post("/login", async(req, res) => {
+//     try {
+//         if(req.body.credential){
+//             const verificationResponse = await verifyGoogleToken(req.body.credential)
+//             if(verificationResponse.error){
+//                 return res.status(400).json({
+//                     message: verificationResponse.error
+//                 })
+//             }
+
+//             const profile = verificationResponse?.payload
+
+//             res.status(201).json({
+//                 message: "Login successful",
+//                 user:{
+//                     firstName: profile?.given_name,
+//                     lastName: profile?.family_name,
+//                     picture: profile?.picture,
+//                     email: profile?.email,
+//                     token: jwt.sign({email: profile?.email}, process.env.JWT_SECRET, {expiresIn: "1d"})
+//                 }
+//             })
+//         }
+//     } catch (error) {
+//         res.status(500).json({message: error?.message || error})
+//     }
+// })
 
 module.exports = oAuthRouter
